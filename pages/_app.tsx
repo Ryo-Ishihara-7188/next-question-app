5e9a5e5d93c50a3a68f529865af99bd42481b521
ishihara <r.funk.7188@gmail.com>
2021-01-05 15:36:32 +0900

    回答機能を実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
-import { RecoilRoot } from "recoil";
-import "../lib/firebase";
-import "../styles/globals.scss";
+import { RecoilRoot } from "recoil"
+import dayjs from "dayjs"
+import "dayjs/locale/ja"
+import "../lib/firebase"
+import "../styles/globals.scss"
+
+dayjs.locale("ja")
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+	return (
+		<RecoilRoot>
+			<Component {...pageProps} />
+		</RecoilRoot>
+	)
 }
 
-export default MyApp;
+export default MyApp
